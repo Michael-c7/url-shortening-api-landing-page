b2f4a7b266400abbaa1538cea252cd5744047ad5
Michael Carr <mcarr2468@gmail.com>
2020-08-10 17:07:34 -0500

    v3.3

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ hamburgerMenu.addEventListener('click', navbarFunctionality);
 
 function navbarFunctionality (event) {
     navbarItemsContainer.classList.toggle('is-open');
-
-    // console.log(event.target);
 }
 
 
@@ -30,18 +28,29 @@ linkShortenerInput.addEventListener('input', linkShortenerInputWarningFunctional
 function linkShortenerInputWarningFunctionality(event) {
     let inputValue = linkShortenerInput.value;
     if(!inputValue) {
-        // apply a class on the input
-        linkShortenerInput.classList.add('warning-msg-on');
+        // makes the placeholder text on the input red
+            linkShortenerInput.classList.add('warning-msg-on');
 
         // show the "please add a link" msg
-        addLinkWarningMsg.classList.add('warning-msg-on');
+            addLinkWarningMsg.classList.add('warning-msg-on');
     
     } else {
-        linkShortenerInput.classList.remove('warning-msg-on');
-        // 
-        addLinkWarningMsg.classList.remove('warning-msg-on');
+        // placeholder text will be the normal color(black)
+            linkShortenerInput.classList.remove('warning-msg-on');
+        // hide the "please add a link" msg
+            addLinkWarningMsg.classList.remove('warning-msg-on');
     }
 }
+
+
+
+
+
+// create the shortened link
+let randomString = Math.random().toString(36).substring(7);
+let completeRandomString = `https://rel.ink/${randomString}`;
+//    console.log(completeRandomString)
+
 
 
 
@@ -53,80 +62,131 @@ linkShortenerOutputContainer.addEventListener('click', (event) => {
     // change styles
     if(clickedTarget.closest('button.btn--copy')) {
         /*change text*/
-        clickedTarget.textContent = 'Copied!';
+            clickedTarget.textContent = 'Copied!';
 
         /*change background color*/
-        /*fallback color */
-        clickedTarget.style.backgroundColor = 'darkslateblue';
-        /*preferred color*/
-        clickedTarget.style.backgroundColor = 'var(--dark-violet)';
+            /*fallback color */
+            clickedTarget.style.backgroundColor = 'darkslateblue';
+            /*preferred color*/
+            clickedTarget.style.backgroundColor = 'var(--dark-violet)';
 
-        // copy target link link to clipboard
-        let targetLink = clickedTarget.parentElement.parentElement;
-        let targetLM = targetLink.querySelector('.link-shortener__output__lm');
-        
-        // console.log(targetLM.textContent);
+        // copy target link link to clipboard (not done)
+            // 1.target link
+            let targetLink = clickedTarget.parentElement.parentElement;
+            let targetLM = targetLink.querySelector('.link-shortener__output__lm');
 
-        
-        
-        // targetLM.innerHTML.select();
-        // console.log(targetLM)
-        targetLM.style.userSelect = 'contain';
-        document.execCommand('Copy');
+            // 2.select the link text
 
-    }
+            // 3. copy target text to clipboard
+
+            // copied from stackoverflow
+
+    //         function selectText(node) {
+    //             node = document.getElementById(node);
+            
+    //             if (document.body.createTextRange) {
+    //                 const range = document.body.createTextRange();
+    //                 range.moveToElementText(node);
+    //                 range.select();
+    //             } else if (window.getSelection) {
+    //                 const selection = window.getSelection();
+    //                 const range = document.createRange();
+    //                 range.selectNodeContents(node);
+    //                 selection.removeAllRanges();
+    //                 selection.addRange(range);
+    //             } else {
+    //                 console.warn("Could not select text in node: Unsupported browser.");
+    //             }
+    //         } 
+
+    //         const clickable = document.querySelector('.click-me');
+    //         clickable.addEventListener('click', () => selectText('target'));
+    //         <div id="target"><p>Some text goes here!</p><p>Moar text!</p></div>
+    //         <p class="click-me">Click me!</p>
+     }
 });
 
-
-
-
-/*shorten link */
+/*shorten link*/
 shortenItButton.addEventListener('click', createLinkShortenerOutput);
 
 
-function createLinkShortenerOutput(shortenedLink) {
+
+
+
+// creates the thing you inject into the DOM
+function createLinkShortenerOutput(originalLink, shortenedLink) {
+    /*below is what we create w/ a document fragment an inject into the DOM */
+
     /*
     <div class="link-shortener__output">
-        <!--link original-->
         <span class="link-shortener__output__lo">https://www.frontendmentor.io</span>
-        <!--link modified-->
-        <span class="link-shortener__output__lm">https://rel.ink/gxOXp9</span>
-        <button class="btn btn--type-2 btn--copy">Copy</button>
+        <div class="link-shortener__lm-container">
+            <span class="link-shortener__output__lm">https://rel.ink/gxOXp9</span>
+            <div class="btn--copy-container">
+                <button class="btn btn--type-2 btn--copy">Copy</button>
+            </div>
+        </div>
     </div> 
     */
 
-   
-   let fragment = new DocumentFragment();
-   let originalLink = `https://www.frontendmentor.io`;
-//    fragment.appendChild(originalLink);
-
-//    console.log(fragment);
-    
-
-    // linkShortenerOutputContainer.insertAdjacentElement("beforeend",fragment);
-    
     /* 
     ***STEPS*** 
     1. create a document fragment of the link-shortener__output
     2. append this to the DOM within the link-shortener__output-container
     */
+
+
+    // 1. create the thing
+        let docFrag = document.createDocumentFragment();
+
+        // link-shortener__output
+        let linkShortenerOutputDOM = document.createElement('div');
+        linkShortenerOutputDOM.classList.add('ink-shortener__output');
+        docFrag.appendChild(linkShortenerOutputDOM);
+
+            // link-shortener__output__lo
+            let linkShortenerOutputLoDOM = document.createElement('span');
+            linkShortenerOutputLoDOM.classList.add('link-shortener__output__lo');
+            linkShortenerOutputLoDOM.textContent = originalLink;
+            linkShortenerOutputDOM.appendChild(linkShortenerOutputLoDOM);
+
+            // link-shortener__lm-container
+            let linkShortenerLmContainerDOM = document.createElement('div');
+            linkShortenerLmContainerDOM.classList.add('link-shortener__lm-container');
+            linkShortenerOutputDOM.appendChild(linkShortenerLmContainerDOM);
+
+                // link-shortener__output__lm
+                let linkShortenerOutputLmDOM = document.createElement('span');
+                linkShortenerOutputLmDOM.classList.add('link-shortener__output__lm');
+                linkShortenerOutputLmDOM.textContent = shortenedLink;
+                linkShortenerLmContainerDOM.appendChild(linkShortenerOutputLmDOM);
+
+                // btn--copy-container
+                let btnCopyContainerDOM = document.createElement('div');
+                btnCopyContainerDOM.classList.add('btn--copy-container');
+                linkShortenerLmContainerDOM.appendChild(btnCopyContainerDOM);
+
+                    // btn--copy
+                    let btnCopy = document.createElement('button');
+                    btnCopy.classList.add('btn');
+                    btnCopy.classList.add('btn--type-2');
+                    btnCopy.classList.add('btn--copy');
+                    btnCopy.textContent = 'Copy';
+                    btnCopyContainerDOM.appendChild(btnCopy);
+
+    // console.log(docFrag);
+
+    /* 2. inject the entire thing into the DOM
+    create unique thing for each thing created.
+    think the unique id should be based
+    on the length of the total of all items
+    OR the use the unique link id as the unique thing
+    for each thing here
+    */
 }
 
-createLinkShortenerOutput();
+createLinkShortenerOutput('userInputHere', completeRandomString);
 
-
-function shortenLink(linkToShorten) {
-   /*
-   from : https://www.frontendmentor.io
-
-   to : https://rel.ink/gxOXp9
-
-   NOTE: the end part
-   eg : gxOXp9 should Not be longer than 6 characters 
-   
-   1. randomly generate a link w/ numbers & letters(lower & uppercase)
-   */
-}
 
 /*
  change link-shortener__output__lm text
